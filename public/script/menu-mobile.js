0cff5ca28f5fd0cf5131cf127dd29f59e1237139
Diegooliveyra <diegooliveirabryto@gmail.com>
2020-12-17 20:43:35 -0300

    corrigido menu mobile no responsivo

--- public/script/menu-mobile.js
+++ public/script/menu-mobile.js
@@ -5,10 +5,12 @@ export default function menuMobile() {
   const allLinksMenu = document.querySelectorAll("[data-navMenu] ul li a");
 
   function menuOpen() {
+    if (window.innerWidth < 768) {
     btnBurger.classList.toggle("menu-btn-open");
     navMenu.classList.toggle("menu-ativo");
     menuUl.classList.toggle("nav-mobile");
   }
+}
 
   allLinksMenu.forEach((link) => {
     link.addEventListener("click", menuOpen);
